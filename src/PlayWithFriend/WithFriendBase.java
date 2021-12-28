23f7bb5a86382cafaf57b42991af8a5166a3c296
Aziza Helmy <aziza.helmy35@gmail.com>
2021-12-27 18:26:01 -0700

    welcome+playwithfriends changes

--- src/PlayWithFriend/WithFriendBase.java
+++ src/PlayWithFriend/WithFriendBase.java
@@ -14,28 +14,32 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import tictactoe.ViewSCreen;
 import static tictactoe.ViewSCreen.gameScreen;
-import static tictactoe.ViewSCreen.onlineScreen;
-import static tictactoe.ViewSCreen.register;
+import static tictactoe.ViewSCreen.welcomescreen;
 
-public  class WithFriendBase extends GridPane {
+public class WithFriendBase extends GridPane {
 
     protected final ColumnConstraints columnConstraints;
     protected final ColumnConstraints columnConstraints0;
     protected final ColumnConstraints columnConstraints1;
     protected final ColumnConstraints columnConstraints2;
+    protected final ColumnConstraints columnConstraints3;
     protected final RowConstraints rowConstraints;
     protected final RowConstraints rowConstraints0;
     protected final RowConstraints rowConstraints1;
     protected final RowConstraints rowConstraints2;
-    protected final Button button;
-    protected final TextField textField;
-    protected final TextField textField0;
+    protected final RowConstraints rowConstraints3;
+    protected final Button btnstart;
+    protected final TextField player2;
+    protected final TextField player1;
     protected final Text text;
     protected final Glow glow;
-    protected final ImageView imageView;
-    protected final ImageView imageView0;
-    protected final ImageView imageView1;
-    protected final ImageView imageView2;
+    protected final ImageView img_friends;
+    protected final ImageView img_player2;
+    protected final ImageView img_player1;
+    protected final ImageView img_start;
+    protected final Button btnBack;
+    protected final ImageView imgBack;
+    protected final Glow glow0;
 
     public WithFriendBase() {
 
@@ -43,19 +47,24 @@ public  class WithFriendBase extends GridPane {
         columnConstraints0 = new ColumnConstraints();
         columnConstraints1 = new ColumnConstraints();
         columnConstraints2 = new ColumnConstraints();
+        columnConstraints3 = new ColumnConstraints();
         rowConstraints = new RowConstraints();
         rowConstraints0 = new RowConstraints();
         rowConstraints1 = new RowConstraints();
         rowConstraints2 = new RowConstraints();
-        button = new Button();
-        textField = new TextField();
-        textField0 = new TextField();
+        rowConstraints3 = new RowConstraints();
+        btnstart = new Button();
+        player2 = new TextField();
+        player1 = new TextField();
         text = new Text();
         glow = new Glow();
-        imageView = new ImageView();
-        imageView0 = new ImageView();
-        imageView1 = new ImageView();
-        imageView2 = new ImageView();
+        img_friends = new ImageView();
+        img_player2 = new ImageView();
+        img_player1 = new ImageView();
+        img_start = new ImageView();
+        btnBack = new Button();
+        imgBack = new ImageView();
+        glow0 = new Glow();
 
         setId("playWithFriendsScreen");
         setMaxHeight(USE_PREF_SIZE);
@@ -70,22 +79,27 @@ public  class WithFriendBase extends GridPane {
         columnConstraints.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
         columnConstraints.setMaxWidth(194.0);
         columnConstraints.setMinWidth(10.0);
-        columnConstraints.setPrefWidth(83.0);
+        columnConstraints.setPrefWidth(49.0);
 
         columnConstraints0.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
-        columnConstraints0.setMaxWidth(212.0);
+        columnConstraints0.setMaxWidth(194.0);
         columnConstraints0.setMinWidth(10.0);
-        columnConstraints0.setPrefWidth(68.0);
+        columnConstraints0.setPrefWidth(62.0);
 
         columnConstraints1.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
-        columnConstraints1.setMaxWidth(354.0);
+        columnConstraints1.setMaxWidth(212.0);
         columnConstraints1.setMinWidth(10.0);
-        columnConstraints1.setPrefWidth(269.0);
+        columnConstraints1.setPrefWidth(76.0);
 
         columnConstraints2.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
-        columnConstraints2.setMaxWidth(180.0);
+        columnConstraints2.setMaxWidth(354.0);
         columnConstraints2.setMinWidth(10.0);
-        columnConstraints2.setPrefWidth(180.0);
+        columnConstraints2.setPrefWidth(298.0);
+
+        columnConstraints3.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
+        columnConstraints3.setMaxWidth(180.0);
+        columnConstraints3.setMinWidth(10.0);
+        columnConstraints3.setPrefWidth(151.0);
 
         rowConstraints.setMinHeight(10.0);
         rowConstraints.setPrefHeight(30.0);
@@ -103,32 +117,42 @@ public  class WithFriendBase extends GridPane {
         rowConstraints2.setPrefHeight(30.0);
         rowConstraints2.setVgrow(javafx.scene.layout.Priority.SOMETIMES);
 
-        GridPane.setColumnIndex(button, 2);
-        GridPane.setRowIndex(button, 3);
-        button.setId("startButt");
-        button.setMnemonicParsing(false);
-        button.setPrefHeight(32.0);
-        button.setPrefWidth(283.0);
-        button.getStylesheets().add("/PlayWithFriend/withfriend.css");
-        button.setText("Start");
-        button.setFont(new Font("Copperplate Gothic Bold", 19.0));
+        rowConstraints3.setMinHeight(10.0);
+        rowConstraints3.setPrefHeight(30.0);
+        rowConstraints3.setVgrow(javafx.scene.layout.Priority.SOMETIMES);
 
-        GridPane.setColumnIndex(textField, 2);
-        GridPane.setRowIndex(textField, 2);
-        textField.setId("player2");
-        textField.setPrefHeight(41.0);
-        textField.setPrefWidth(200.0);
-        textField.setPromptText("            Enter Player2's Name :");
-        textField.getStylesheets().add("/PlayWithFriend/withfriend.css");
+        GridPane.setColumnIndex(btnstart, 3);
+        GridPane.setRowIndex(btnstart, 3);
+        btnstart.setId("startButt");
+        btnstart.setMnemonicParsing(false);
+        btnstart.setPrefHeight(32.0);
+        btnstart.setPrefWidth(283.0);
+        btnstart.getStylesheets().add("/PlayWithFriend/withfriend.css");
+        btnstart.setText("Start");
+        btnstart.setFont(new Font("Copperplate Gothic Bold", 19.0));
+        btnstart.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ViewSCreen.view(gameScreen);
+            }
+        });
+//===============================================================
+        GridPane.setColumnIndex(player2, 3);
+        GridPane.setRowIndex(player2, 2);
+        player2.setId("player2");
+        player2.setPrefHeight(41.0);
+        player2.setPrefWidth(280.0);
+        player2.setPromptText("            Enter Player2's Name :");
+        player2.getStylesheets().add("/PlayWithFriend/withfriend.css");
 
-        GridPane.setColumnIndex(textField0, 2);
-        GridPane.setRowIndex(textField0, 1);
-        textField0.setId("player1");
-        textField0.setPrefHeight(41.0);
-        textField0.setPrefWidth(200.0);
-        textField0.setPromptText("         Enter Player1's Name :");
+        GridPane.setColumnIndex(player1, 3);
+        GridPane.setRowIndex(player1, 1);
+        player1.setId("player1");
+        player1.setPrefHeight(41.0);
+        player1.setPrefWidth(200.0);
+        player1.setPromptText("         Enter Player1's Name :");
 
-        GridPane.setColumnIndex(text, 2);
+        GridPane.setColumnIndex(text, 3);
         text.setFontSmoothingType(javafx.scene.text.FontSmoothingType.LCD);
         text.setLineSpacing(3.0);
         text.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
@@ -139,71 +163,95 @@ public  class WithFriendBase extends GridPane {
         text.setEffect(glow);
         text.setFont(new Font("Berlin Sans FB Demi Bold", 23.0));
 
-        GridPane.setColumnIndex(imageView, 3);
-        imageView.setFitHeight(120.0);
-        imageView.setFitWidth(155.0);
-        imageView.setPickOnBounds(true);
-        imageView.setPreserveRatio(true);
+        GridPane.setColumnIndex(img_friends, 4);
+        img_friends.setFitHeight(120.0);
+        img_friends.setFitWidth(155.0);
+        img_friends.setPickOnBounds(true);
+        img_friends.setPreserveRatio(true);
         try {
-            imageView.setImage(new Image(getClass().getResource("/assets/icons8-user-groups-100.png").toExternalForm()));
+            img_friends.setImage(new Image(getClass().getResource("/assets/icons8-user-groups-100.png").toExternalForm()));
         } catch (Exception e) {
         }
 
-        GridPane.setColumnIndex(imageView0, 2);
-        GridPane.setRowIndex(imageView0, 2);
-        imageView0.setFitHeight(47.0);
-        imageView0.setFitWidth(81.0);
-        imageView0.setNodeOrientation(javafx.geometry.NodeOrientation.RIGHT_TO_LEFT);
-        imageView0.setPickOnBounds(true);
-        imageView0.setPreserveRatio(true);
+        GridPane.setColumnIndex(img_player2, 3);
+        GridPane.setRowIndex(img_player2, 2);
+        img_player2.setFitHeight(47.0);
+        img_player2.setFitWidth(81.0);
+        img_player2.setNodeOrientation(javafx.geometry.NodeOrientation.RIGHT_TO_LEFT);
+        img_player2.setPickOnBounds(true);
+        img_player2.setPreserveRatio(true);
         try {
-            imageView0.setImage(new Image(getClass().getResource("/assets/icons8-football-team-48.png").toExternalForm()));
+            img_player2.setImage(new Image(getClass().getResource("/assets/icons8-football-team-48.png").toExternalForm()));
         } catch (Exception e) {
         }
 
-        GridPane.setColumnIndex(imageView1, 2);
-        GridPane.setRowIndex(imageView1, 1);
-        imageView1.setFitHeight(50.0);
-        imageView1.setFitWidth(32.0);
-        imageView1.setPickOnBounds(true);
-        imageView1.setPreserveRatio(true);
+        GridPane.setColumnIndex(img_player1, 3);
+        GridPane.setRowIndex(img_player1, 1);
+        img_player1.setFitHeight(50.0);
+        img_player1.setFitWidth(32.0);
+        img_player1.setPickOnBounds(true);
+        img_player1.setPreserveRatio(true);
         try {
-            imageView1.setImage(new Image(getClass().getResource("/assets/icons8-team-96.png").toExternalForm()));
+            img_player1.setImage(new Image(getClass().getResource("/assets/icons8-team-96.png").toExternalForm()));
         } catch (Exception e) {
         }
 
-        GridPane.setColumnIndex(imageView2, 1);
-        GridPane.setRowIndex(imageView2, 3);
-        imageView2.setFitHeight(56.0);
-        imageView2.setFitWidth(108.0);
-        imageView2.setPickOnBounds(true);
-        imageView2.setPreserveRatio(true);
+        GridPane.setColumnIndex(img_start, 4);
+        GridPane.setRowIndex(img_start, 3);
+        img_start.setFitHeight(56.0);
+        img_start.setFitWidth(108.0);
+        img_start.setPickOnBounds(true);
+        img_start.setPreserveRatio(true);
         try {
-            imageView2.setImage(new Image(getClass().getResource("/assets/icons8-game-controller-96.png").toExternalForm()));
+            img_start.setImage(new Image(getClass().getResource("/assets/icons8-game-controller-96.png").toExternalForm()));
         } catch (Exception e) {
         }
+//======================================================
+        GridPane.setColumnIndex(btnBack, 1);
+        GridPane.setRowIndex(btnBack, 4);
+        btnBack.setAlignment(javafx.geometry.Pos.CENTER);
+        btnBack.setMnemonicParsing(false);
+        btnBack.setPrefHeight(37.0);
+        btnBack.setPrefWidth(98.0);
+        btnBack.getStylesheets().add("/PlayWithFriend/withfriend.css");
+        btnBack.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ViewSCreen.view(welcomescreen);
+            }
+        });
+//===================================================
+        imgBack.setFitHeight(36.0);
+        imgBack.setFitWidth(40.0);
+        imgBack.setPickOnBounds(true);
+        imgBack.setPreserveRatio(true);
+        try {
+            imgBack.setImage(new Image(getClass().getResource("/assets/previous.png").toExternalForm()));
+        } catch (Exception e) {
+        }
+        btnBack.setGraphic(imgBack);
+
+        btnBack.setEffect(glow0);
 
         getColumnConstraints().add(columnConstraints);
         getColumnConstraints().add(columnConstraints0);
         getColumnConstraints().add(columnConstraints1);
         getColumnConstraints().add(columnConstraints2);
+        getColumnConstraints().add(columnConstraints3);
         getRowConstraints().add(rowConstraints);
         getRowConstraints().add(rowConstraints0);
         getRowConstraints().add(rowConstraints1);
         getRowConstraints().add(rowConstraints2);
-        getChildren().add(button);
-        getChildren().add(textField);
-        getChildren().add(textField0);
+        getRowConstraints().add(rowConstraints3);
+        getChildren().add(btnstart);
+        getChildren().add(player2);
+        getChildren().add(player1);
         getChildren().add(text);
-        getChildren().add(imageView);
-        getChildren().add(imageView0);
-        getChildren().add(imageView1);
-        getChildren().add(imageView2);
-button.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                ViewSCreen.view(gameScreen);
-            }
-        });
+        getChildren().add(img_friends);
+        getChildren().add(img_player2);
+        getChildren().add(img_player1);
+        getChildren().add(img_start);
+        getChildren().add(btnBack);
+
     }
 }
